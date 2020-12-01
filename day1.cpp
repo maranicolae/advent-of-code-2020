fff8ada4dcdc41cb2a34495973e3af5ccd1ecd2a
maranicolae <55808861+maranicolae@users.noreply.github.com>
2020-12-01 14:19:17 +0200

    Add files via upload

---
+++ day1.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> two_sum(vector<int>& nums, int target) {
+    unordered_map<int, int> um;
+    vector<int> res;
+
+    // Map element to its index
+    for (int i = 0; i < nums.size(); ++i) {
+        um[nums[i]] = i;
+    }
+
+    for (int i = 0; i < nums.size(); ++i) {
+
+        // Check if the complement exists in the map
+        auto it = um.find(target - nums[i]);
+        if (it != um.end() && it->second != i) {
+            res.push_back(nums[i]);
+            res.push_back(nums[it->second]);
+            break;
+        }
+    }
+
+    return res;
+}
+
+int three_sum(vector<int>& nums, int target) {
+    for (int i = 0; i < nums.size(); i++) {
+        // Check if the complement can be built by using two other elements
+        auto res = two_sum(nums, target - nums[i]);
+        if (res.size()) {
+            return nums[i] * res[0] * res[1];
+        }
+    }
+    return 0;
+}
+
+int main() {
+    ifstream in;
+    in.open("input");
+    std::vector<int> nums;
+
+    while(!in.eof()) {
+        int num;
+        in >> num;
+        nums.push_back(num);
+    }
+
+    std::cout << three_sum(nums, 2020) << "\n";
+    in.close();
+}
